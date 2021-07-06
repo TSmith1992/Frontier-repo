05ab9b9e6a303f604c09e9dbca9fcc0712c9418a
Tarique Smith <tpsmith1992@gmail.com>
2021-07-06 09:15:56 -0400

    Add Header with Buttons

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript"; 
 
 document.querySelector('body').appendChild(h2); 
+
+const btn=document.getElementById('button');
+
+btn.addEventListener('click',function(){
+    if (btn.style.backgroundColor=''){
+        btn.style.backgroundColor='#27647B';
+    }else btn.style.backgroundColor='green';
+});
